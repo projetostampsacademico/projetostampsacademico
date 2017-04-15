2af03814aa05020331f8d87f45b8b6638d450c6b
ftuyama <ftuyama@gmail.com>
2017-04-15 17:04:38 -0300

    Updates

--- medicos/web/web/settings.py
+++ medicos/web/web/settings.py
@@ -87,6 +87,9 @@ DATABASES = {
 # Enable Heroku DATABASE
 if 'SOURCE_VERSION' in os.environ or 'WEB_CONCURRENCY' in os.environ:
     # Parse database configuration from $DATABASE_URL
+    import dj_database_url
+
+    #DATABASES['default'] = dj_database_url.config() 
     pprint("HEEEEEEEEEEEEEEEEREEEEEEEEEEEEEEEEEEE")
     pprint("HEEEEEEEEEEEEEEEEREEEEEEEEEEEEEEEEEEE")
     pprint("HEEEEEEEEEEEEEEEEREEEEEEEEEEEEEEEEEEE")
