8d22f92cf5e4f80f32fa6021f9c6acd040766f19
ftuyama <ftuyama@gmail.com>
2017-04-21 00:27:31 -0300

    MongoDB screen

--- medicos/web/nosql/views.py
+++ medicos/web/nosql/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
+from pymongo import MongoClient
 
 # Create your views here.
 
-def index(request):
+def index(request, template_name='nosql/index.html'):
     """Index page."""
-    return render_to_response(
-        'web/index.html', context_instance=RequestContext(request))
+    client = MongoClient('mongodb://heroku_6c04n1s3:eg0rtjdqtmf8ukmmo3pfflqdbe@ds161630.mlab.com:61630/heroku_6c04n1s3')
+    db = client.heroku_6c04n1s3
+    collections = db.collection_names()
+    data = []
+    for d in db.stamps.find():
+        data.append(d)
+    return render(request, template_name, {'data': data, 'collections': collections})
