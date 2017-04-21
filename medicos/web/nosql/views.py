3bb495498f2ec69120a391823ffef762a200ca15
ftuyama <ftuyama@gmail.com>
2017-04-21 00:41:52 -0300

    Refactors mongodb service

--- medicos/web/nosql/views.py
+++ medicos/web/nosql/views.py
@@ -1,15 +1,12 @@
 from django.shortcuts import render, render_to_response
 from django.template import RequestContext
-from pymongo import MongoClient
+from nosql.service import MongoService
 
 # Create your views here.
 
 def index(request, template_name='nosql/index.html'):
     """Index page."""
-    client = MongoClient('mongodb://heroku_6c04n1s3:eg0rtjdqtmf8ukmmo3pfflqdbe@ds161630.mlab.com:61630/heroku_6c04n1s3')
-    db = client.heroku_6c04n1s3
-    collections = db.collection_names()
-    data = []
-    for d in db.stamps.find():
-        data.append(d)
+    service = MongoService()
+    collections = service.collection_names()
+    data = service.fetch_data()
     return render(request, template_name, {'data': data, 'collections': collections})
