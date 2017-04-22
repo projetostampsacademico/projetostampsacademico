2182e20ef7adbf11bb9104d0a8a2b42c8203c8a6
Fabio Kfouri <kfouri.fabio@gmail.com>
2017-04-22 18:47:32 -0300

    Correção do tradutor

--- medicos/local/translate.py
+++ medicos/local/translate.py
@@ -5,10 +5,13 @@ import urllib.request as rq
 
 
 def translate(text):
+    
+    '''
     proxy_handler = rq.ProxyHandler({"http": "http://login:pwd@lnx237in.sjk.emb:9090"})
     proxy_opener = rq.build_opener(proxy_handler)
-    # gs = goslate.Goslate()
     gs = goslate.Goslate(opener=proxy_opener)
+    '''
+    gs = goslate.Goslate()
     return gs.translate('give me some love', 'pt-br')
 
 
