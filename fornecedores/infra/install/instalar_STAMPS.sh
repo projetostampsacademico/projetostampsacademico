6f609f51eed45862e79e7bc2e587809f45d21ac3
mgc <marcelogc@gmail.com>
2017-05-18 14:49:23 -0300

    instalacao do hadoop hive

--- fornecedores/infra/install/instalar_STAMPS.sh
+++ fornecedores/infra/install/instalar_STAMPS.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 clear
 
@@ -11,97 +11,105 @@ dialog --yesno 'Atualizar o Linux?' 5 40
         	apt-get upgrade	 
 	fi
 
+
+
+dialog --yesno 'Instalar MYSQL utilizado em todos os servidores?'  5 40
+         if [ $? = 0 ]; then
+		clear
+		echo "instalando Mysql"
+        	apt-get install mysql-server
+        	mysql_secure_installation
+	 fi
+
 dialog --yesno 'Instalar Standard system utilities?'  5 40 
 	 if [ $? = 0 ]; then 
 		clear;
-        	apt-get install aptitude vim manpages dnsutils bsdmainutils psmisc python3-gdbm ufw dosfstools ed telnet powermgmt-base ntfs-3g ubuntu-release-upgrader-core iputils-tracepath groff-base bind9-host mtr-tiny bash-completion mlocate tcpdump geoip-database install-info irqbalance language-selector-common friendly-recovery command-not-found info hdparm man-db lshw update-manager-core apt-transport-https accountsservice command-not-found-data time ltrace parted popularity-contest strace ftp ubuntu-standard lsof glances  python-software-properties nodejs
+        	apt-get install aptitude vim manpages dnsutils bsdmainutils psmisc ufw dosfstools ed telnet powermgmt-base ntfs-3g ubuntu-release-upgrader-core iputils-tracepath groff-base bind9-host mtr-tiny bash-completion mlocate tcpdump geoip-database install-info irqbalance language-selector-common friendly-recovery command-not-found info hdparm man-db lshw update-manager-core apt-transport-https accountsservice command-not-found-data time ltrace parted popularity-contest strace ftp ubuntu-standard lsof glances  python-software-properties nodejs rsync
 	 fi
 
-SERVER=$( dialog --stdout --title 'Qual Servidor'        \
-        --menu '\nQual servidor esta instalando?\n\n'  \
-        0 0 0                                    \
-        API_WEB        'SERVIDOR 1 - API + WEB'  \
-        BANCO_DADOS    'SERVIDOR 2 - BANCO DE DADOS' \ )
-
-users=$( dialog --stdout --separate-output --title 'Criar Usuários'        \
-   	--checklist '\nQuais Usuários deseja criar?\n\n'  \
-   	0 0 0                                    \
-   	1  'Time Scrum 01 - ts01'      off    \
-	2  'Time Scrum 02 - ts02'      off    \
-	3  'Time Scrum 03 - ts03'      off    \
-	4  'Time Scrum 04 - ts04'      off    \ )
-
-echo "$users" | while read USERID
-do
-
-	clear
-	echo "##########################################"
-	echo "##				        ##"
-	echo "## Criando Conta do TIME 0$USERID             ##"
-	echo "##                                      ##"
-	echo "##########################################"
-
-	USER="ts0$USERID"
-	if grep -qs $USER /etc/passwd; then continue; fi
-
-	useradd -s /bin/bash -m -d /home/$USER -c "SIG-TRAC - $USER"  -u 800$USERID  $USER
-
-	sudo -u $USER mkdir /home/$USER/.ssh
-	sudo -u $USER ssh-keygen -f /home/$USER/.ssh/key#$USER#$SERVER
-	sudo -u $USER cat /home/$USER/.ssh/key#$USER#$SERVER.pub >> /home/$USER/.ssh/authorized_keys
-
-	echo $USER >> .tmpUser
+SERVER=$( dialog --stdout --title 'Qual Servidor'       \
+        --menu '\nQual servidor esta instalando?\n\n'  	\
+        0 0 0                                    	\
+        API_WEB        'API + WEB'  			\
+	PROCESSAMENTO  'PROCESSAMENTO' 			  )
 
 
-done
-
-
-if [ $SERVER = "BANCO_DADOS" ];then
-
-	ip=$(ifconfig | sed -n '/inet addr:/{p;q;}' | sed 's/ [^ ]\+ *$//g' | sed 's/ [^ ]\+ *$//g' | sed 's/^.*://')
-  	clear
-	echo "instalando Mysql"
-	apt-get install mysql-server
-	mysql_secure_installation
-	sudo service mysql stop
-	mv /etc/mysql/my.cnf /etc/mysql/my.cnf.old 
-	cat /etc/mysql/my.cnf.old  | sed "s/127.0.0.1/$ip/g" > /etc/mysql/my.cnf
-	sudo service mysql start
-
-	clear
-	echo "instalando MongoDB"
-	aptitude install mongodb
-	service mongodb stop
-	cp /etc/mongodb.conf  /etc/mongodb.conf.old
-	cat /etc/mongodb.conf.old | sed "s/127.0.0.1/$ip/g" > /etc/mongodb.conf
-	service mongodb start
-fi
-
-if [ $SERVER = "API_WEB" ];then
+if [[ $SERVER = "API_WEB" ]];then
 	clear
 	echo "instalando python"
 	aptitude install python-software-properties python-setuptools python-pip
 	pip install --upgrade pip
 	pip install virtualenv
+	
+	echo "instalando LAMP"
+	apt-get install apache2 
+	apt-get install php libapache2-mod-php php-mcrypt php-mysql 
+
 fi
 
 
-if [ $SERVER = "PROCESSAMENTO" ];then
+if [[ $SERVER = "PROCESSAMENTO" ]];then
 	clear
-	echo "instalando Spark"
+	echo "instalando Hadoop"
 	aptitude install python-software-properties
 	add-apt-repository ppa:webupd8team/java
 	aptitude update
 	aptitude install oracle-java8-installer oracle-java8-set-default
         echo "JAVA_HOME=/usr/lib/jvm/java-8-oracle" >> /etc/environment
         source /etc/environment
-	cd /opt/
-	wget http://d3kbcqa49mib13.cloudfront.net/spark-1.6.1-bin-hadoop2.6.tgz
-	tar -xzvf spark-1.6.1-bin-hadoop2.6.tgz
-	ln -s spark-1.6.1-bin-hadoop2.6  spark
-	echo "SPARK_HOME=/opt/spark" >> /etc/environment
-	echo 'PATH=$PATH:/opt/spark/bin/' >> /etc/environment
-	source /etc/environment
+	useradd -d /usr/local/hadoop -m -s /bin/bash hadoop
+	echo "Adicionar senha no Hadoop"
+	passwd hadoop
+	adduser hadoop sudo
+	echo "Preparando HDFS"
+        mkdir /hadoop
+        chown hadoop:hadoop  /hadoop
+
+	echo "Criando Chave"
+	sudo -i -u hadoop ssh-keygen
+	sudo -i -u hadoop ssh-copy-id hadoop@localhost
+	echo "instalando Hadoop"
+	sudo -i -u hadoop wget http://ftp.unicamp.br/pub/apache/hadoop/common/hadoop-2.8.0/hadoop-2.8.0.tar.gz
+	sudo -i -u hadoop tar -xzf hadoop-2.8.0.tar.gz
+	sudo -i -u hadoop ln -s /usr/local/hadoop/hadoop-2.8.0 /usr/local/hadoop/hadoop
+	echo "HADOOP_HOME=/usr/local/hadoop/hadoop" >> 	/etc/environment
+	echo "configurando Hadoop"
+	chown hadoop:hadoop *.xml
+	cp ./core-site.xml /usr/local/hadoop/hadoop/etc/hadoop/
+        cp ./hdfs-site.xml /usr/local/hadoop/hadoop/etc/hadoop/
+	echo "Formatando HDFS"
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/bin/hdfs namenode -format
+	echo "configurando Yarn"
+	cp ./mapred-site.xml /usr/local/hadoop/hadoop/etc/hadoop/
+	cp ./yarn-site.xml  /usr/local/hadoop/hadoop/etc/hadoop/
+	echo "instalando o hive"
+	sudo -i -u hadoop wget http://ftp.unicamp.br/pub/apache/hive/hive-1.2.2/apache-hive-1.2.2-bin.tar.gz
+	sudo -i -u hadoop tar -xzf apache-hive-1.2.2-bin.tar.gz
+	sudo -i -u hadoop ln -s /usr/local/hadoop/apache-hive-1.2.2-bin /usr/local/hadoop/hive
+	echo "HIVE_HOME=/usr/local/hadoop/hive" >>  /etc/environment
+
+	apt-get install libmysql-java
+	sudo -i -u hadoop ln -s /usr/share/java/mysql-connector-java.jar /usr/local/hadoop/hive/lib/mysql-connector-java.jar
+	cd /usr/local/hadoop/hive/scripts/metastore/upgrade/mysql
+	mysql -u root -p  -e "CREATE DATABASE metastore CHARACTER SET utf8 COLLATE utf8_general_ci";
+	mysql -u root -p metastore < hive-schema-0.14.0.mysql.sql
+
+	mysql -u root -p -e " CREATE USER 'hiveuser'@'%' IDENTIFIED BY 'hivepassword';"
+	mysql -u root -p -e " GRANT all on metastore.* to 'hiveuser'@'%' identified by 'hivepassword';"
+	mysql -u root -p -e " flush privileges"
+
+	cp ./hive-default.xml /usr/local/hadoop/hive/conf
+
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/sbin/start-dfs.sh 
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/sbin/start-yarn.sh
+
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/bin/hdfs dfs -mkdir       /tmp
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/bin/hdfs dfs -mkdir       /user
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/bin/hdfs dfs -mkdir	     /user/hive/
+        sudo -i -u hadoop /usr/local/hadoop/hadoop/bin/hdfs dfs -mkdir       /user/hive/warehouse
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/bin/hdfs dfs -chmod g+w   /tmp
+	sudo -i -u hadoop /usr/local/hadoop/hadoop/bin/hdfs dfs -chmod g+w   /user/hive/warehouse
+ 			
 fi 
 
 
