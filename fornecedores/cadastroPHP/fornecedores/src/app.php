8ddd99fb8273839e986d791e531f3a97e1546c19
marcelogumercinocosta <marcelogc@gmail.com>
2017-05-30 20:34:30 -0300

    cadastro + upload NFE US17

---
+++ fornecedores/cadastroPHP/fornecedores/src/app.php
@@ -0,0 +1,36 @@
+<?php
+
+use Silex\Application;
+use Silex\Provider\AssetServiceProvider;
+use Silex\Provider\TwigServiceProvider;
+use Silex\Provider\ServiceControllerServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
+
+
+$app = new Application();
+$app->register(new ServiceControllerServiceProvider());
+$app->register(new AssetServiceProvider());
+$app->register(new TwigServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+
+$app['twig'] = $app->extend('twig', function ($twig, $app) {
+    // add custom globals, filters, tags, ...
+
+    return $twig;
+});
+
+##### bd
+$app->register(new Silex\Provider\DoctrineServiceProvider(), array(
+    'dbs.options' => array (
+        'mysql_read' => array(
+            'driver'    => 'pdo_mysql',
+            'host'      => '127.0.0.1',
+            'dbname'    => 'fornecedores',
+            'user'      => 'root',
+            'password'  => '123456',
+            'charset'   => 'utf8mb4',
+        ),
+    ),
+));
+
+return $app;
