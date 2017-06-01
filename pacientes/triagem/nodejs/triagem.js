604d40c7d47867e9aba626e4fe78610abab88973
bassetoe <EBasseto@lhs-systems.com>
2017-06-01 14:18:53 -0300

    Estrutura padrao para validacao

--- pacientes/triagem/nodejs/triagem.js
+++ pacientes/triagem/nodejs/triagem.js
@@ -1,26 +1,40 @@
 var kafka = require('kafka-node');
+var patientcontroller = require('./KafkaControllers/patientController');
 
 var client = new kafka.Client(process.env.ZOOKEEPER || '34.204.88.242:2181');
 
 var offset = new kafka.Offset(client); // <<< PARA INICIAR A LEITURA A PARTIR DA ULTIMA MENSAGEM
-var consumer = new kafka.Consumer(client,[{ topic: process.env.TOPICIN || 'det-twitter', offset: offset }]);
+var consumer = new kafka.Consumer(client, [{
+  topic: process.env.TOPICIN || 'det-twitter',
+  offset: offset
+}]);
 var producer = new kafka.Producer(client);
+console.log("Listening to topic: " + process.env.TOPICIN + " ...");
+consumer.on('message', function (message) { // <<< REALIZA A LEITURA DO TOPICO ANTERIOR
 
-consumer.on('message', function(message) { // <<< REALIZA A LEITURA DO TOPICO ANTERIOR
+  //Somente envia ao próximo tópico se algum validador "validar".
+  //Deve-se criar um controller e um modelo para cada canonico que será enviado ao tópico.
+  var isValid = false;
 
-  console.log('TRIAGEM'); // <<< LOCAL ONDE DEVE OCORRE A TRIAGEM
+  if (patientcontroller.PatientValidator(message.value))
+    isValid = true;
 
-  producer.send([{ topic: process.env.TOPICOUT || 'tri-twitter', messages: [message.value] }], function (err, result) { // <<< ENVIA PARA O TOPICO POSTERIOR
-    console.log(err || result);
-  });
+
+  if (isValid) {
+    producer.send([{
+      topic: process.env.TOPICOUT || 'tri-twitter',
+      messages: [message.value]
+    }], function (err, result) { // <<< ENVIA PARA O TOPICO POSTERIOR
+      console.log(err || result);
+    });
+  }
+
 });
 
-consumer.on('error', function(err) {
+consumer.on('error', function (err) {
   console.log('consumer error', err);
 });
 
-producer.on('error', function(err) {
+producer.on('error', function (err) {
   console.log('producer error', err);
 });
-
-
