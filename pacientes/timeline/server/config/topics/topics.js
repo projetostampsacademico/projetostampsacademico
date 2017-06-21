9b6687b8816a622889eb32edf125fa7b03ce7f1c
Edizon Basseto <edizon.basseto@outlook.com>
2017-06-20 23:01:32 -0300

    config dos topicos

---
+++ pacientes/timeline/server/config/topics/topics.js
@@ -0,0 +1,27 @@
+  module.exports = {
+      "test": {
+          "icon": "syringe.png",
+          "fontcolor": "white",
+          "backgroundcolor": "red"
+      },
+      "tri-twitter": {
+          "icon": "twitter.png",
+          "fontcolor": "black",
+          "backgroundcolor": "#99ccff"
+      },
+      "tri-paciente": {
+          "icon": "syringe.png",
+          "fontcolor": "black",
+          "backgroundcolor": "#99ff99"
+      },
+      "tri-hospital": {
+          "icon": "hospital.png",
+          "fontcolor": "white",
+          "backgroundcolor": "#669999"
+      },
+      "tri-medico": {
+          "icon": "doctor.png",
+          "fontcolor": "black",
+          "backgroundcolor": "#ffffff"
+      }
+  }
