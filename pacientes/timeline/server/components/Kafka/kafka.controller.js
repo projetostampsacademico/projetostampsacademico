f75e47de5364fddbeccdc4c529afe738cbb8f2a7
Edizon Basseto <edizon.basseto@outlook.com>
2017-06-20 23:01:48 -0300

    ler topicos baseado no config

--- pacientes/timeline/server/components/Kafka/kafka.controller.js
+++ pacientes/timeline/server/components/Kafka/kafka.controller.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var kafka = require('kafka-node');
+var configtopics = require('../../config/topics/topics');
 
 exports.startListen = function (topicName) {
 
@@ -10,28 +11,66 @@ exports.startListen = function (topicName) {
     global.client = new kafka.Client(process.env.ZOOKEEPER || '34.204.88.242:2181');
     global.offset = new kafka.Offset(client); // <<< PARA INICIAR A LEITURA A PARTIR DA ULTIMA MENSAGEM
 
-    global.consumer = new kafka.Consumer(client, [
-        { topic: 'tri-twitter', offset: offset },
-        { topic: 'tri-paciente', offset: offset },
-        { topic: 'tri-hospital', offset: offset },
-        { topic: 'tri-medico', offset: offset },
-        { topic: 'test', offset: offset}
-    ]);
+    var topics = [];
+
+    for (var topic in configtopics) {
+        topics.push({
+            topic: topic,
+            offset: offset
+        });
+    }
+
+    if(topics.length < 1){
+        console.log("Missing topics configuration. Broker communication cannot be stabilished.")
+        return;
+    }
+    console.log(topics);
+    /*
+    [{
+            topic: 'tri-twitter',
+            offset: offset
+        },
+        {
+            topic: 'tri-paciente',
+            offset: offset
+        },
+        {
+            topic: 'tri-hospital',
+            offset: offset
+        },
+        {
+            topic: 'tri-medico',
+            offset: offset
+        },
+        {
+            topic: 'test',
+            offset: offset
+        }
+    ]
+    */
+    global.consumer = new kafka.Consumer(client, topics);
 
     consumer.on('message', function (message) {
+        console.log(message);
         pushData(message);
     });
 
 };
 
 function pushData(message) {
-    if(lendodados) {
-        setTimeout(pushData, 50);//wait 50 millisecnds then recheck
+    if (lendodados) {
+        setTimeout(pushData, 50); //wait 50 millisecnds then recheck
         return;
     }
-    
+
     if (message.value && !lendodados) {
-            dados.push(message.value);
+
+        dados.push({
+            backgroundcolor: configtopics[message.topic].backgroundcolor,
+            icon: configtopics[message.topic].icon,
+            fontcolor: configtopics[message.topic].fontcolor || 'black',
+            message: message.value
+        });
     }
 
 }
