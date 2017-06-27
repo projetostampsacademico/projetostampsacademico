6b9a5d2102e9997cbc3b8c5a13a9367e81c47421
Edizon Basseto <edizon.basseto@outlook.com>
2017-06-27 00:04:55 +0000

    Ajustes para multiplas sessions

--- pacientes/timeline/server/api/broker/broker.controller.js
+++ pacientes/timeline/server/api/broker/broker.controller.js
@@ -1,19 +1,30 @@
 'use strict';
 
-
+var session = require('express-session');
 var resposta = [];
 
 exports.listAll = function(req, res){
     lendodados = true;
-    if(dados.length > 0){
-        var retorno = dados.concat();
-        dados = [];
-        lendodados = false;
-        res.status(200).send(retorno);
+    var sess = req.session;
+    sess.retorno = [];
+    sess.contador = 0;
+    if(!sess.posicao) {
+        sess.posicao = 0;
+    }
+
+    if(dados.length > 0 && dados.length != sess.posicao){
+        
+        console.log("sess.posicao = ", sess.posicao);
+        console.log("dados.length = ", dados.length);
+        for(var x=sess.posicao; x<dados.length; x++){
+            sess.retorno.push(dados[x]);
+            sess.contador++;
+        }
+        sess.posicao += sess.contador;
     }
     
     lendodados = false;
-    res.status(200).send();
+    res.status(200).send(sess.retorno);
 
 };
 
--- pacientes/timeline/server/components/Kafka/kafka.controller.js
+++ pacientes/timeline/server/components/Kafka/kafka.controller.js
@@ -1,54 +1,51 @@
-'use strict';
-
-var kafka = require('kafka-node');
-var configtopics = require('../../config/topics/topics');
-
-exports.startListen = function (topicName) {
-
-    // globals
-    global.dados = [];
-    global.lendodados = false;
-    global.client = new kafka.Client(process.env.ZOOKEEPER || '34.204.88.242:2181');
-    global.offset = new kafka.Offset(client); // <<< PARA INICIAR A LEITURA A PARTIR DA ULTIMA MENSAGEM
-
-    var topics = [];
-
-    for (var topic in configtopics) {
-        topics.push({
-            topic: topic,
-            offset: offset
-        });
-    }
-
-    if(topics.length < 1){
-        console.log("Missing topics configuration. Broker communication cannot be stabilished.")
-        return;
-    }
-    console.log(topics);
-    global.consumer = new kafka.Consumer(client, topics);
-
-    consumer.on('message', function (message) {
-        console.log(message);
-        pushData(message);
-    });
-
-};
-
-function pushData(message) {
-    if (lendodados) {
-        setTimeout(pushData, 50); //wait 50 millisecnds then recheck
-        return;
-    }
-
-    if (message.value && !lendodados) {
-
-        dados.push({
-            display: configtopics[message.topic].display,
-            backgroundcolor: configtopics[message.topic].backgroundcolor,
-            icon: configtopics[message.topic].icon,
-            fontcolor: configtopics[message.topic].fontcolor || 'black',
-            message: message.value
-        });
-    }
-
+'use strict';
+
+var kafka = require('kafka-node');
+var configtopics = require('../../config/topics/topics');
+
+exports.startListen = function (topicName) {
+    // globals
+    global.dados = [];
+    global.lendodados = false;
+    global.client = new kafka.Client(process.env.ZOOKEEPER || '34.204.88.242:2181');
+    var offset = new kafka.Offset(client); // <<< PARA INICIAR A LEITURA A PARTIR DA ULTIMA MENSAGEM
+    var topics = [];
+
+    for (var topic in configtopics) {
+        topics.push({
+            topic: topic,
+            offset: offset
+        });
+    }
+
+    if (topics.length < 1) {
+        console.log("Missing topics configuration. Broker communication cannot be stabilished.")
+        return;
+    }
+
+    console.log(topics);
+    global.consumer = new kafka.Consumer(client, topics);
+
+    consumer.on('message', function (message) {
+        console.log(message);
+        pushData(message);
+    });
+
+};
+
+function pushData(message) {
+    if (lendodados) {
+        setTimeout(pushData, 50); //wait 50 millisecnds then recheck
+        return;
+    }
+    if (message.value && !lendodados) {
+        dados.push({
+            display: configtopics[message.topic].display,
+            backgroundcolor: configtopics[message.topic].backgroundcolor,
+            icon: configtopics[message.topic].icon,
+            fontcolor: configtopics[message.topic].fontcolor || 'black',
+            message: message.value
+        });
+    }
+
 }
