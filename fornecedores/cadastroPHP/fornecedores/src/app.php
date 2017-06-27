fe15d4bb631b8107cd47cfc1e90d0b72d622018a
Marcelo Gumercino Costa <mgc@MacBook-Pro-de-Marcelo.local>
2017-06-26 21:54:47 -0300

    Adicionando Template

--- fornecedores/cadastroPHP/fornecedores/src/app.php
+++ fornecedores/cadastroPHP/fornecedores/src/app.php
@@ -9,15 +9,14 @@ use Silex\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
+
 $app->register(new AssetServiceProvider());
-$app->register(new TwigServiceProvider());
 $app->register(new HttpFragmentServiceProvider());
-
+$app->register(new TwigServiceProvider());
 $app['twig'] = $app->extend('twig', function ($twig, $app) {
-    // add custom globals, filters, tags, ...
-
     return $twig;
 });
+
 
 ##### bd
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
@@ -27,7 +26,7 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
             'host'      => '127.0.0.1',
             'dbname'    => 'fornecedores',
             'user'      => 'root',
-            'password'  => '123456',
+            'password'  => '123456789',
             'charset'   => 'utf8mb4',
         ),
     ),
