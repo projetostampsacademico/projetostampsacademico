5f8a98780f98b97950460687c3477a72b81bfcc2
Edizon Basseto <edizon.basseto@outlook.com>
2017-06-27 00:27:20 +0000

    Ajustes de posicao

--- pacientes/timeline/client/site/js/components/maps/maps.controller.js
+++ pacientes/timeline/client/site/js/components/maps/maps.controller.js
@@ -4,7 +4,7 @@ angular.module('stampsacad')
     .controller('mapsCtrl', ['$rootScope', '$scope', 'NgMap', function ($rootScope, $scope, NgMap) {
 
         $scope.center = [-23.20878, -45.85637];
-
+        $scope.markers=[];
         $scope.zoomtomark=false;
         //BROADCAST Recebido com dados novos para LAT LONG
         $rootScope.$on('latlong-recebido', function (event, args) {
@@ -17,7 +17,7 @@ angular.module('stampsacad')
             $scope.center = [args.position.lat, args.position.long];
             if($scope.markers.length > 3)
             {
-                zoomtomark = true;
+                $scope.zoomtomark = true;
             }
 
         });
